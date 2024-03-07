b639200e313647f397b08de8f158fd9d28adc72d
lucasalberti <lucasfranciscoalberti@gmail.com>
2024-03-07 15:06:04 -0300

    finalização

--- js/main.js
+++ js/main.js
@@ -77,7 +77,11 @@ function alterarContexto(contexto){
 const contagemRegressiva = () =>{
     if(tempoDecorridoEmSegundos <= 0){
         audioTempoFinalizado.play()
-        alert('Tempo finalizado!')
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if(focoAtivo){
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
         zerar()
         return
     }
@@ -116,6 +120,10 @@ function mostrarTempo(){
 }
 
 mostrarTempo()
+
+
+
+
 
 
 
